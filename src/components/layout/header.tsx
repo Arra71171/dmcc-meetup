d6ad2a1f8870c6f8f768b67efbc0187b14228398
Culture Code <team.culturecode@gmail.com>
2025-06-08 03:34:29 +0000

    let's use the same dark color in the project in the navbar as well

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -45,7 +45,7 @@ export function Header() {
 
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-white/80 dark:bg-black/80 backdrop-blur supports-[backdrop-filter]:bg-white/50 dark:supports-[backdrop-filter]:bg-black/50">
+    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-white/80 dark:bg-background/80 backdrop-blur supports-[backdrop-filter]:bg-white/50 dark:supports-[backdrop-filter]:bg-background/50">
       <div className="container flex h-16 max-w-screen-2xl items-center justify-between px-4 md:px-6">
         <Logo />
         <nav className="hidden md:flex items-center space-x-1 text-sm font-semibold">
